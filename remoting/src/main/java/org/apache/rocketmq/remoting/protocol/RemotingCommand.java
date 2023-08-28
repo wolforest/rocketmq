593f771d6f4cf881cdc861e53317a0f7844e01ec
winglechen <winglechen@gmail.com>
2023-08-28 20:05:26 +0800

    feature: add setAndReturn method to RemotingCommand

--- remoting/src/main/java/org/apache/rocketmq/remoting/protocol/RemotingCommand.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/protocol/RemotingCommand.java
@@ -251,6 +251,12 @@ public class RemotingCommand {
         return (type.getCode() << 24) | (source & 0x00FFFFFF);
     }
 
+    public RemotingCommand setAndReturn(int code, String remark) {
+        setCode(code);
+        setRemark(remark);
+        return this;
+    }
+
     public void markResponseType() {
         int bits = 1 << RPC_TYPE;
         this.flag |= bits;
