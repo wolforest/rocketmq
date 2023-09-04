e1eb32910a3796c751ee0e4ef6f7334135c7a357
winglechen <winglechen@gmail.com>
2023-09-04 21:28:04 +0800

    refactor: simplify DefaultMQPushConsumerImpl.makeSureStateOK

--- client/src/main/java/org/apache/rocketmq/client/impl/consumer/DefaultMQPushConsumerImpl.java
+++ client/src/main/java/org/apache/rocketmq/client/impl/consumer/DefaultMQPushConsumerImpl.java
@@ -730,12 +730,14 @@ public class DefaultMQPushConsumerImpl implements MQConsumerInner {
     }
 
     private void makeSureStateOK() throws MQClientException {
-        if (this.serviceState != ServiceState.RUNNING) {
-            throw new MQClientException("The consumer service state not OK, "
-                + this.serviceState
-                + FAQUrl.suggestTodo(FAQUrl.CLIENT_SERVICE_NOT_OK),
-                null);
+        if (this.serviceState == ServiceState.RUNNING) {
+            return;
         }
+
+        throw new MQClientException("The consumer service state not OK, "
+            + this.serviceState
+            + FAQUrl.suggestTodo(FAQUrl.CLIENT_SERVICE_NOT_OK),
+            null);
     }
 
     void executePullRequestLater(final PullRequest pullRequest, final long timeDelay) {
