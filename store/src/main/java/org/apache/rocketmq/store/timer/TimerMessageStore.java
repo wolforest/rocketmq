6d6f39713ec26d2339f42f68f8f26792a3874799
yik <tech.yiguo@gmail.com>
2023-09-17 11:34:56 +0800

    feat:recover logic in TimerMessageRecover

--- store/src/main/java/org/apache/rocketmq/store/timer/TimerMessageStore.java
+++ store/src/main/java/org/apache/rocketmq/store/timer/TimerMessageStore.java
@@ -50,6 +50,7 @@ import org.apache.rocketmq.store.logfile.MappedFile;
 import org.apache.rocketmq.store.stats.BrokerStatsManager;
 import org.apache.rocketmq.store.timer.service.MessageOperator;
 import org.apache.rocketmq.store.timer.service.TimerMessageQuery;
+import org.apache.rocketmq.store.timer.service.TimerMessageRecover;
 import org.apache.rocketmq.store.timer.service.TimerMetricManager;
 import org.apache.rocketmq.store.timer.service.TimerWheelFetcher;
 import org.apache.rocketmq.store.timer.service.TimerMessageDeliver;
@@ -104,6 +105,7 @@ public class TimerMessageStore {
     private Function<MessageExtBrokerInner, PutMessageResult> escapeBridgeHook;
     private MessageOperator messageOperator;
     private TimerMetricManager timerMetricManager;
+    private TimerMessageRecover recover;
 
     public TimerMessageStore(final MessageStore messageStore, final MessageStoreConfig storeConfig,
                              TimerCheckpoint timerCheckpoint, TimerMetrics timerMetrics,
@@ -222,7 +224,12 @@ public class TimerMessageStore {
                 timerCheckpoint,
                 timerLog,
                 timerWheel);
-
+        recover = new TimerMessageRecover(
+                timerState,
+                timerWheel,
+                timerLog,
+                messageOperator,
+                timerCheckpoint);
 
     }
 
@@ -230,7 +237,7 @@ public class TimerMessageStore {
         this.initService();
         boolean load = timerLog.load();
         load = load && this.timerMetrics.load();
-        recover();
+        recover.recover();
         calcTimerDistribution();
         return load;
     }
@@ -260,70 +267,6 @@ public class TimerMessageStore {
         }
         long endTime = System.currentTimeMillis();
         LOGGER.debug("Total cost Time: {}", endTime - startTime);
-    }
-
-
-
-    //recover timerLog and revise timerWheel
-    //return process offset
-    private long recoverAndRevise(long beginOffset, boolean checkTimerLog) {
-        LOGGER.info("Begin to recover timerLog offset:{} check:{}", beginOffset, checkTimerLog);
-        MappedFile lastFile = timerLog.getMappedFileQueue().getLastMappedFile();
-        if (null == lastFile) {
-            return 0;
-        }
-
-        List<MappedFile> mappedFiles = timerLog.getMappedFileQueue().getMappedFiles();
-        int index = mappedFiles.size() - 1;
-        for (; index >= 0; index--) {
-            MappedFile mappedFile = mappedFiles.get(index);
-            if (beginOffset >= mappedFile.getFileFromOffset()) {
-                break;
-            }
-        }
-        if (index < 0) {
-            index = 0;
-        }
-        long checkOffset = mappedFiles.get(index).getFileFromOffset();
-        for (; index < mappedFiles.size(); index++) {
-            MappedFile mappedFile = mappedFiles.get(index);
-            SelectMappedBufferResult sbr = mappedFile.selectMappedBuffer(0, checkTimerLog ? mappedFiles.get(index).getFileSize() : mappedFile.getReadPosition());
-            ByteBuffer bf = sbr.getByteBuffer();
-            int position = 0;
-            boolean stopCheck = false;
-            for (; position < sbr.getSize(); position += TimerLog.UNIT_SIZE) {
-                try {
-                    bf.position(position);
-                    int size = bf.getInt();//size
-                    bf.getLong();//prev pos
-                    int magic = bf.getInt();
-                    if (magic == TimerLog.BLANK_MAGIC_CODE) {
-                        break;
-                    }
-                    if (checkTimerLog && (!TimerState.isMagicOK(magic) || TimerLog.UNIT_SIZE != size)) {
-                        stopCheck = true;
-                        break;
-                    }
-                    long delayTime = bf.getLong() + bf.getInt();
-                    if (TimerLog.UNIT_SIZE == size && TimerState.isMagicOK(magic)) {
-                        timerWheel.reviseSlot(delayTime, TimerWheel.IGNORE, sbr.getStartOffset() + position, true);
-                    }
-                } catch (Exception e) {
-                    LOGGER.error("Recover timerLog error", e);
-                    stopCheck = true;
-                    break;
-                }
-            }
-            sbr.release();
-            checkOffset = mappedFiles.get(index).getFileFromOffset() + position;
-            if (stopCheck) {
-                break;
-            }
-        }
-        if (checkTimerLog) {
-            timerLog.getMappedFileQueue().truncateDirtyFiles(checkOffset);
-        }
-        return checkOffset;
     }
 
     public void start() {
@@ -610,119 +553,6 @@ public class TimerMessageStore {
 
     public TimerState getTimerState() {
         return timerState;
-    }
-
-
-    @SuppressWarnings("NonAtomicOperationOnVolatileField")
-    public void recover() {
-        //recover timerLog
-        long lastFlushPos = timerCheckpoint.getLastTimerLogFlushPos();
-        MappedFile lastFile = timerLog.getMappedFileQueue().getLastMappedFile();
-        if (null != lastFile) {
-            lastFlushPos = lastFlushPos - lastFile.getFileSize();
-        }
-        if (lastFlushPos < 0) {
-            lastFlushPos = 0;
-        }
-        long processOffset = recoverAndRevise(lastFlushPos, true);
-
-        timerLog.getMappedFileQueue().setFlushedWhere(processOffset);
-        //revise queue offset
-        long queueOffset = reviseQueueOffset(processOffset);
-        if (-1 == queueOffset) {
-            timerState.currQueueOffset = timerCheckpoint.getLastTimerQueueOffset();
-        } else {
-            timerState.currQueueOffset = queueOffset + 1;
-        }
-        timerState.currQueueOffset = Math.min(timerState.currQueueOffset, timerCheckpoint.getMasterTimerQueueOffset());
-
-        //check timer wheel
-        timerState.currReadTimeMs = timerCheckpoint.getLastReadTimeMs();
-        long nextReadTimeMs = formatTimeMs(
-                System.currentTimeMillis()) - (long) timerState.slotsTotal * precisionMs + (long) TimerState.TIMER_BLANK_SLOTS * precisionMs;
-        if (timerState.currReadTimeMs < nextReadTimeMs) {
-            timerState.currReadTimeMs = nextReadTimeMs;
-        }
-        //the timer wheel may contain physical offset bigger than timerLog
-        //This will only happen when the timerLog is damaged
-        //hard to test
-        long minFirst = timerWheel.checkPhyPos(timerState.currReadTimeMs, processOffset);
-        if (debug) {
-            minFirst = 0;
-        }
-        if (minFirst < processOffset) {
-            LOGGER.warn("Timer recheck because of minFirst:{} processOffset:{}", minFirst, processOffset);
-            recoverAndRevise(minFirst, false);
-        }
-        LOGGER.info("Timer recover ok currReadTimerMs:{} currQueueOffset:{} checkQueueOffset:{} processOffset:{}",
-                timerState.currReadTimeMs, timerState.currQueueOffset, timerCheckpoint.getLastTimerQueueOffset(), processOffset);
-
-        timerState.commitReadTimeMs = timerState.currReadTimeMs;
-        timerState.commitQueueOffset = timerState.currQueueOffset;
-
-        timerState.prepareTimerCheckPoint();
-    }
-
-    public long reviseQueueOffset(long processOffset) {
-        SelectMappedBufferResult selectRes = timerLog.getTimerMessage(processOffset - (TimerLog.UNIT_SIZE - TimerLog.UNIT_PRE_SIZE_FOR_MSG));
-        if (null == selectRes) {
-            return -1;
-        }
-        try {
-            long offsetPy = selectRes.getByteBuffer().getLong();
-            int sizePy = selectRes.getByteBuffer().getInt();
-            MessageExt messageExt = messageOperator.readMessageByCommitOffset(offsetPy, sizePy);
-            if (null == messageExt) {
-                return -1;
-            }
-
-            // check offset in msg is equal to offset of cq.
-            // if not, use cq offset.
-            long msgQueueOffset = messageExt.getQueueOffset();
-            int queueId = messageExt.getQueueId();
-            ConsumeQueue cq = (ConsumeQueue) messageOperator.getConsumeQueue(TIMER_TOPIC, queueId);
-            if (null == cq) {
-                return msgQueueOffset;
-            }
-            long cqOffset = msgQueueOffset;
-            long tmpOffset = msgQueueOffset;
-            int maxCount = 20000;
-            while (maxCount-- > 0) {
-                if (tmpOffset < 0) {
-                    LOGGER.warn("reviseQueueOffset check cq offset fail, msg in cq is not found.{}, {}",
-                            offsetPy, sizePy);
-                    break;
-                }
-                SelectMappedBufferResult bufferCQ = cq.getIndexBuffer(tmpOffset);
-                if (null == bufferCQ) {
-                    // offset in msg may be greater than offset of cq.
-                    tmpOffset -= 1;
-                    continue;
-                }
-                try {
-                    long offsetPyTemp = bufferCQ.getByteBuffer().getLong();
-                    int sizePyTemp = bufferCQ.getByteBuffer().getInt();
-                    if (offsetPyTemp == offsetPy && sizePyTemp == sizePy) {
-                        LOGGER.info("reviseQueueOffset check cq offset ok. {}, {}, {}",
-                                tmpOffset, offsetPyTemp, sizePyTemp);
-                        cqOffset = tmpOffset;
-                        break;
-                    }
-                    tmpOffset -= 1;
-                } catch (Throwable e) {
-                    LOGGER.error("reviseQueueOffset check cq offset error.", e);
-                } finally {
-                    bufferCQ.release();
-                }
-            }
-
-            return cqOffset;
-        } finally {
-            selectRes.release();
-        }
-    }
-    private long formatTimeMs(long timeMs) {
-        return timeMs / precisionMs * precisionMs;
     }
 
 
--- store/src/main/java/org/apache/rocketmq/store/timer/service/TimerMessageRecover.java
+++ store/src/main/java/org/apache/rocketmq/store/timer/service/TimerMessageRecover.java
@@ -29,6 +29,9 @@ import org.apache.rocketmq.store.timer.TimerLog;
 import org.apache.rocketmq.store.timer.TimerState;
 import org.apache.rocketmq.store.timer.TimerWheel;
 
+import java.nio.ByteBuffer;
+import java.util.List;
+
 public class TimerMessageRecover {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggerName.STORE_LOGGER_NAME);
     public static final String TIMER_TOPIC = TopicValidator.SYSTEM_TOPIC_PREFIX + "wheel_timer";
@@ -41,8 +44,190 @@ public class TimerMessageRecover {
     private boolean debug = false;
     private int precisionMs;
 
-    public TimerMessageRecover(){
+    public TimerMessageRecover(TimerState timerState,TimerWheel timerWheel,TimerLog timerLog,MessageOperator messageOperator,TimerCheckpoint timerCheckpoint){
+        this.timerState = timerState;
+        this.timerWheel = timerWheel;
+        this.timerLog = timerLog;
+        this.messageOperator = messageOperator;
+        this.timerCheckpoint = timerCheckpoint;
         precisionMs = timerState.precisionMs;
     }
+
+
+    @SuppressWarnings("NonAtomicOperationOnVolatileField")
+    public void recover() {
+        //recover timerLog
+        long lastFlushPos = timerCheckpoint.getLastTimerLogFlushPos();
+        MappedFile lastFile = timerLog.getMappedFileQueue().getLastMappedFile();
+        if (null != lastFile) {
+            lastFlushPos = lastFlushPos - lastFile.getFileSize();
+        }
+        if (lastFlushPos < 0) {
+            lastFlushPos = 0;
+        }
+        long processOffset = recoverAndRevise(lastFlushPos, true);
+
+        timerLog.getMappedFileQueue().setFlushedWhere(processOffset);
+        //revise queue offset
+        long queueOffset = reviseQueueOffset(processOffset);
+        if (-1 == queueOffset) {
+            timerState.currQueueOffset = timerCheckpoint.getLastTimerQueueOffset();
+        } else {
+            timerState.currQueueOffset = queueOffset + 1;
+        }
+        timerState.currQueueOffset = Math.min(timerState.currQueueOffset, timerCheckpoint.getMasterTimerQueueOffset());
+
+        //check timer wheel
+        timerState.currReadTimeMs = timerCheckpoint.getLastReadTimeMs();
+        long nextReadTimeMs = formatTimeMs(
+                System.currentTimeMillis()) - (long) timerState.slotsTotal * precisionMs + (long) TimerState.TIMER_BLANK_SLOTS * precisionMs;
+        if (timerState.currReadTimeMs < nextReadTimeMs) {
+            timerState.currReadTimeMs = nextReadTimeMs;
+        }
+        //the timer wheel may contain physical offset bigger than timerLog
+        //This will only happen when the timerLog is damaged
+        //hard to test
+        long minFirst = timerWheel.checkPhyPos(timerState.currReadTimeMs, processOffset);
+        if (debug) {
+            minFirst = 0;
+        }
+        if (minFirst < processOffset) {
+            LOGGER.warn("Timer recheck because of minFirst:{} processOffset:{}", minFirst, processOffset);
+            recoverAndRevise(minFirst, false);
+        }
+        LOGGER.info("Timer recover ok currReadTimerMs:{} currQueueOffset:{} checkQueueOffset:{} processOffset:{}",
+                timerState.currReadTimeMs, timerState.currQueueOffset, timerCheckpoint.getLastTimerQueueOffset(), processOffset);
+
+        timerState.commitReadTimeMs = timerState.currReadTimeMs;
+        timerState.commitQueueOffset = timerState.currQueueOffset;
+
+        timerState.prepareTimerCheckPoint();
+    }
+
+    //recover timerLog and revise timerWheel
+    //return process offset
+    private long recoverAndRevise(long beginOffset, boolean checkTimerLog) {
+        LOGGER.info("Begin to recover timerLog offset:{} check:{}", beginOffset, checkTimerLog);
+        MappedFile lastFile = timerLog.getMappedFileQueue().getLastMappedFile();
+        if (null == lastFile) {
+            return 0;
+        }
+
+        List<MappedFile> mappedFiles = timerLog.getMappedFileQueue().getMappedFiles();
+        int index = mappedFiles.size() - 1;
+        for (; index >= 0; index--) {
+            MappedFile mappedFile = mappedFiles.get(index);
+            if (beginOffset >= mappedFile.getFileFromOffset()) {
+                break;
+            }
+        }
+        if (index < 0) {
+            index = 0;
+        }
+        long checkOffset = mappedFiles.get(index).getFileFromOffset();
+        for (; index < mappedFiles.size(); index++) {
+            MappedFile mappedFile = mappedFiles.get(index);
+            SelectMappedBufferResult sbr = mappedFile.selectMappedBuffer(0, checkTimerLog ? mappedFiles.get(index).getFileSize() : mappedFile.getReadPosition());
+            ByteBuffer bf = sbr.getByteBuffer();
+            int position = 0;
+            boolean stopCheck = false;
+            for (; position < sbr.getSize(); position += TimerLog.UNIT_SIZE) {
+                try {
+                    bf.position(position);
+                    int size = bf.getInt();//size
+                    bf.getLong();//prev pos
+                    int magic = bf.getInt();
+                    if (magic == TimerLog.BLANK_MAGIC_CODE) {
+                        break;
+                    }
+                    if (checkTimerLog && (!TimerState.isMagicOK(magic) || TimerLog.UNIT_SIZE != size)) {
+                        stopCheck = true;
+                        break;
+                    }
+                    long delayTime = bf.getLong() + bf.getInt();
+                    if (TimerLog.UNIT_SIZE == size && TimerState.isMagicOK(magic)) {
+                        timerWheel.reviseSlot(delayTime, TimerWheel.IGNORE, sbr.getStartOffset() + position, true);
+                    }
+                } catch (Exception e) {
+                    LOGGER.error("Recover timerLog error", e);
+                    stopCheck = true;
+                    break;
+                }
+            }
+            sbr.release();
+            checkOffset = mappedFiles.get(index).getFileFromOffset() + position;
+            if (stopCheck) {
+                break;
+            }
+        }
+        if (checkTimerLog) {
+            timerLog.getMappedFileQueue().truncateDirtyFiles(checkOffset);
+        }
+        return checkOffset;
+    }
+
+
+    public long reviseQueueOffset(long processOffset) {
+        SelectMappedBufferResult selectRes = timerLog.getTimerMessage(processOffset - (TimerLog.UNIT_SIZE - TimerLog.UNIT_PRE_SIZE_FOR_MSG));
+        if (null == selectRes) {
+            return -1;
+        }
+        try {
+            long offsetPy = selectRes.getByteBuffer().getLong();
+            int sizePy = selectRes.getByteBuffer().getInt();
+            MessageExt messageExt = messageOperator.readMessageByCommitOffset(offsetPy, sizePy);
+            if (null == messageExt) {
+                return -1;
+            }
+
+            // check offset in msg is equal to offset of cq.
+            // if not, use cq offset.
+            long msgQueueOffset = messageExt.getQueueOffset();
+            int queueId = messageExt.getQueueId();
+            ConsumeQueue cq = (ConsumeQueue) messageOperator.getConsumeQueue(TIMER_TOPIC, queueId);
+            if (null == cq) {
+                return msgQueueOffset;
+            }
+            long cqOffset = msgQueueOffset;
+            long tmpOffset = msgQueueOffset;
+            int maxCount = 20000;
+            while (maxCount-- > 0) {
+                if (tmpOffset < 0) {
+                    LOGGER.warn("reviseQueueOffset check cq offset fail, msg in cq is not found.{}, {}",
+                            offsetPy, sizePy);
+                    break;
+                }
+                SelectMappedBufferResult bufferCQ = cq.getIndexBuffer(tmpOffset);
+                if (null == bufferCQ) {
+                    // offset in msg may be greater than offset of cq.
+                    tmpOffset -= 1;
+                    continue;
+                }
+                try {
+                    long offsetPyTemp = bufferCQ.getByteBuffer().getLong();
+                    int sizePyTemp = bufferCQ.getByteBuffer().getInt();
+                    if (offsetPyTemp == offsetPy && sizePyTemp == sizePy) {
+                        LOGGER.info("reviseQueueOffset check cq offset ok. {}, {}, {}",
+                                tmpOffset, offsetPyTemp, sizePyTemp);
+                        cqOffset = tmpOffset;
+                        break;
+                    }
+                    tmpOffset -= 1;
+                } catch (Throwable e) {
+                    LOGGER.error("reviseQueueOffset check cq offset error.", e);
+                } finally {
+                    bufferCQ.release();
+                }
+            }
+
+            return cqOffset;
+        } finally {
+            selectRes.release();
+        }
+    }
+    private long formatTimeMs(long timeMs) {
+        return timeMs / precisionMs * precisionMs;
+    }
+
 
 }
