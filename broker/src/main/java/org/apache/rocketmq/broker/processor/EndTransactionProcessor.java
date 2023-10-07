de1927ab13e9ee9f17dd1dde79867ce7a26bc119
winglechen <winglechen@gmail.com>
2023-10-07 21:24:31 +0800

    refactor: simplify EndTransactionProcessor

--- broker/src/main/java/org/apache/rocketmq/broker/processor/EndTransactionProcessor.java
+++ broker/src/main/java/org/apache/rocketmq/broker/processor/EndTransactionProcessor.java
@@ -42,6 +42,11 @@ import org.apache.rocketmq.store.config.BrokerRole;
 
 /**
  * EndTransaction processor: process commit and rollback message
+ * prepare message will be processed by SendMessageProcessor
+ *
+ * There are two more groups of threads:
+ *      1. to check prepared message status
+ *      2. to send check message to client
  */
 public class EndTransactionProcessor implements NettyRequestProcessor {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggerName.TRANSACTION_LOGGER_NAME);
@@ -70,7 +75,9 @@ public class EndTransactionProcessor implements NettyRequestProcessor {
         OperationResult result = new OperationResult();
         if (MessageSysFlag.TRANSACTION_COMMIT_TYPE == requestHeader.getCommitOrRollback()) {
             return processCommitRequest(requestHeader, response);
-        } else if (MessageSysFlag.TRANSACTION_ROLLBACK_TYPE == requestHeader.getCommitOrRollback()) {
+        }
+
+        if (MessageSysFlag.TRANSACTION_ROLLBACK_TYPE == requestHeader.getCommitOrRollback()) {
             return processRollbackRequest(requestHeader, response);
         }
 
@@ -231,30 +238,31 @@ public class EndTransactionProcessor implements NettyRequestProcessor {
 
     private RemotingCommand checkPrepareMessage(MessageExt msgExt, EndTransactionRequestHeader requestHeader) {
         final RemotingCommand response = RemotingCommand.createResponseCommand(null);
-        if (msgExt != null) {
-            final String pgroupRead = msgExt.getProperty(MessageConst.PROPERTY_PRODUCER_GROUP);
-            if (!pgroupRead.equals(requestHeader.getProducerGroup())) {
-                response.setCode(ResponseCode.SYSTEM_ERROR);
-                response.setRemark("The producer group wrong");
-                return response;
-            }
-
-            if (msgExt.getQueueOffset() != requestHeader.getTranStateTableOffset()) {
-                response.setCode(ResponseCode.SYSTEM_ERROR);
-                response.setRemark("The transaction state table offset wrong");
-                return response;
-            }
-
-            if (msgExt.getCommitLogOffset() != requestHeader.getCommitLogOffset()) {
-                response.setCode(ResponseCode.SYSTEM_ERROR);
-                response.setRemark("The commit log offset wrong");
-                return response;
-            }
-        } else {
+        if (msgExt == null) {
             response.setCode(ResponseCode.SYSTEM_ERROR);
             response.setRemark("Find prepared transaction message failed");
             return response;
         }
+
+        final String pgroupRead = msgExt.getProperty(MessageConst.PROPERTY_PRODUCER_GROUP);
+        if (!pgroupRead.equals(requestHeader.getProducerGroup())) {
+            response.setCode(ResponseCode.SYSTEM_ERROR);
+            response.setRemark("The producer group wrong");
+            return response;
+        }
+
+        if (msgExt.getQueueOffset() != requestHeader.getTranStateTableOffset()) {
+            response.setCode(ResponseCode.SYSTEM_ERROR);
+            response.setRemark("The transaction state table offset wrong");
+            return response;
+        }
+
+        if (msgExt.getCommitLogOffset() != requestHeader.getCommitLogOffset()) {
+            response.setCode(ResponseCode.SYSTEM_ERROR);
+            response.setRemark("The commit log offset wrong");
+            return response;
+        }
+
         response.setCode(ResponseCode.SUCCESS);
         return response;
     }
@@ -272,9 +280,10 @@ public class EndTransactionProcessor implements NettyRequestProcessor {
         msgInner.setWaitStoreMsgOK(false);
         msgInner.setTransactionId(msgExt.getUserProperty(MessageConst.PROPERTY_UNIQ_CLIENT_MESSAGE_ID_KEYIDX));
         msgInner.setSysFlag(msgExt.getSysFlag());
-        TopicFilterType topicFilterType =
-            (msgInner.getSysFlag() & MessageSysFlag.MULTI_TAGS_FLAG) == MessageSysFlag.MULTI_TAGS_FLAG ? TopicFilterType.MULTI_TAG
+        TopicFilterType topicFilterType = (msgInner.getSysFlag() & MessageSysFlag.MULTI_TAGS_FLAG) == MessageSysFlag.MULTI_TAGS_FLAG
+                ? TopicFilterType.MULTI_TAG
                 : TopicFilterType.SINGLE_TAG;
+
         long tagsCodeValue = MessageExtBrokerInner.tagsString2tagsCode(topicFilterType, msgInner.getTags());
         msgInner.setTagsCode(tagsCodeValue);
         MessageAccessor.setProperties(msgInner, msgExt.getProperties());
@@ -287,72 +296,71 @@ public class EndTransactionProcessor implements NettyRequestProcessor {
     private RemotingCommand sendFinalMessage(MessageExtBrokerInner msgInner) {
         final RemotingCommand response = RemotingCommand.createResponseCommand(null);
         final PutMessageResult putMessageResult = this.brokerController.getMessageStore().putMessage(msgInner);
-        if (putMessageResult != null) {
-            switch (putMessageResult.getPutMessageStatus()) {
-                // Success
-                case PUT_OK:
-                case FLUSH_DISK_TIMEOUT:
-                case FLUSH_SLAVE_TIMEOUT:
-                case SLAVE_NOT_AVAILABLE:
-                    response.setCode(ResponseCode.SUCCESS);
-                    response.setRemark(null);
-                    break;
-                // Failed
-                case CREATE_MAPPED_FILE_FAILED:
-                    response.setCode(ResponseCode.SYSTEM_ERROR);
-                    response.setRemark("Create mapped file failed.");
-                    break;
-                case MESSAGE_ILLEGAL:
-                case PROPERTIES_SIZE_EXCEEDED:
-                    response.setCode(ResponseCode.MESSAGE_ILLEGAL);
-                    response.setRemark(String.format("The message is illegal, maybe msg body or properties length not matched. msg body length limit %dB, msg properties length limit 32KB.",
-                        this.brokerController.getMessageStoreConfig().getMaxMessageSize()));
-                    break;
-                case SERVICE_NOT_AVAILABLE:
-                    response.setCode(ResponseCode.SERVICE_NOT_AVAILABLE);
-                    response.setRemark("Service not available now.");
-                    break;
-                case OS_PAGE_CACHE_BUSY:
-                    response.setCode(ResponseCode.SYSTEM_ERROR);
-                    response.setRemark("OS page cache busy, please try another machine");
-                    break;
-                case WHEEL_TIMER_MSG_ILLEGAL:
-                    response.setCode(ResponseCode.MESSAGE_ILLEGAL);
-                    response.setRemark(String.format("timer message illegal, the delay time should not be bigger than the max delay %dms; or if set del msg, the delay time should be bigger than the current time",
-                        this.brokerController.getMessageStoreConfig().getTimerMaxDelaySec() * 1000L));
-                    break;
-                case WHEEL_TIMER_FLOW_CONTROL:
-                    response.setCode(ResponseCode.SYSTEM_ERROR);
-                    response.setRemark(String.format("timer message is under flow control, max num limit is %d or the current value is greater than %d and less than %d, trigger random flow control",
-                        this.brokerController.getMessageStoreConfig().getTimerCongestNumEachSlot() * 2L, this.brokerController.getMessageStoreConfig().getTimerCongestNumEachSlot(), this.brokerController.getMessageStoreConfig().getTimerCongestNumEachSlot() * 2L));
-                    break;
-                case WHEEL_TIMER_NOT_ENABLE:
-                    response.setCode(ResponseCode.SYSTEM_ERROR);
-                    response.setRemark(String.format("accurate timer message is not enabled, timerWheelEnable is %s",
-                        this.brokerController.getMessageStoreConfig().isTimerWheelEnable()));
-                    break;
-                case UNKNOWN_ERROR:
-                    response.setCode(ResponseCode.SYSTEM_ERROR);
-                    response.setRemark("UNKNOWN_ERROR");
-                    break;
-                case IN_SYNC_REPLICAS_NOT_ENOUGH:
-                    response.setCode(ResponseCode.SYSTEM_ERROR);
-                    response.setRemark("in-sync replicas not enough");
-                    break;
-                case PUT_TO_REMOTE_BROKER_FAIL:
-                    response.setCode(ResponseCode.SYSTEM_ERROR);
-                    response.setRemark("put to remote broker fail");
-                    break;
-                default:
-                    response.setCode(ResponseCode.SYSTEM_ERROR);
-                    response.setRemark("UNKNOWN_ERROR DEFAULT");
-                    break;
-            }
-            return response;
-        } else {
-            response.setCode(ResponseCode.SYSTEM_ERROR);
-            response.setRemark("store putMessage return null");
+        if (putMessageResult == null) {
+            return response.setCodeAndRemark(ResponseCode.SYSTEM_ERROR, "store putMessage return null");
+        }
+
+        switch (putMessageResult.getPutMessageStatus()) {
+            // Success
+            case PUT_OK:
+            case FLUSH_DISK_TIMEOUT:
+            case FLUSH_SLAVE_TIMEOUT:
+            case SLAVE_NOT_AVAILABLE:
+                response.setCode(ResponseCode.SUCCESS);
+                response.setRemark(null);
+                break;
+            // Failed
+            case CREATE_MAPPED_FILE_FAILED:
+                response.setCode(ResponseCode.SYSTEM_ERROR);
+                response.setRemark("Create mapped file failed.");
+                break;
+            case MESSAGE_ILLEGAL:
+            case PROPERTIES_SIZE_EXCEEDED:
+                response.setCode(ResponseCode.MESSAGE_ILLEGAL);
+                response.setRemark(String.format("The message is illegal, maybe msg body or properties length not matched. msg body length limit %dB, msg properties length limit 32KB.",
+                    this.brokerController.getMessageStoreConfig().getMaxMessageSize()));
+                break;
+            case SERVICE_NOT_AVAILABLE:
+                response.setCode(ResponseCode.SERVICE_NOT_AVAILABLE);
+                response.setRemark("Service not available now.");
+                break;
+            case OS_PAGE_CACHE_BUSY:
+                response.setCode(ResponseCode.SYSTEM_ERROR);
+                response.setRemark("OS page cache busy, please try another machine");
+                break;
+            case WHEEL_TIMER_MSG_ILLEGAL:
+                response.setCode(ResponseCode.MESSAGE_ILLEGAL);
+                response.setRemark(String.format("timer message illegal, the delay time should not be bigger than the max delay %dms; or if set del msg, the delay time should be bigger than the current time",
+                    this.brokerController.getMessageStoreConfig().getTimerMaxDelaySec() * 1000L));
+                break;
+            case WHEEL_TIMER_FLOW_CONTROL:
+                response.setCode(ResponseCode.SYSTEM_ERROR);
+                response.setRemark(String.format("timer message is under flow control, max num limit is %d or the current value is greater than %d and less than %d, trigger random flow control",
+                    this.brokerController.getMessageStoreConfig().getTimerCongestNumEachSlot() * 2L, this.brokerController.getMessageStoreConfig().getTimerCongestNumEachSlot(), this.brokerController.getMessageStoreConfig().getTimerCongestNumEachSlot() * 2L));
+                break;
+            case WHEEL_TIMER_NOT_ENABLE:
+                response.setCode(ResponseCode.SYSTEM_ERROR);
+                response.setRemark(String.format("accurate timer message is not enabled, timerWheelEnable is %s",
+                    this.brokerController.getMessageStoreConfig().isTimerWheelEnable()));
+                break;
+            case UNKNOWN_ERROR:
+                response.setCode(ResponseCode.SYSTEM_ERROR);
+                response.setRemark("UNKNOWN_ERROR");
+                break;
+            case IN_SYNC_REPLICAS_NOT_ENOUGH:
+                response.setCode(ResponseCode.SYSTEM_ERROR);
+                response.setRemark("in-sync replicas not enough");
+                break;
+            case PUT_TO_REMOTE_BROKER_FAIL:
+                response.setCode(ResponseCode.SYSTEM_ERROR);
+                response.setRemark("put to remote broker fail");
+                break;
+            default:
+                response.setCode(ResponseCode.SYSTEM_ERROR);
+                response.setRemark("UNKNOWN_ERROR DEFAULT");
+                break;
         }
         return response;
+
     }
 }
