94836bae320fcf5c103d6a0742eac6c203a3246d
wenliang <twlls2011@163.com>
2023-10-10 16:59:44 +0800

    refactor: UtilAll.deleteFile to IOTinyUtils

--- broker/src/test/java/org/apache/rocketmq/broker/controller/ReplicasManagerRegisterTest.java
+++ broker/src/test/java/org/apache/rocketmq/broker/controller/ReplicasManagerRegisterTest.java
@@ -18,14 +18,13 @@
 package org.apache.rocketmq.broker.controller;
 
 import org.apache.rocketmq.broker.BrokerController;
+import org.apache.rocketmq.broker.out.BrokerOuterAPI;
 import org.apache.rocketmq.broker.service.BrokerClusterService;
 import org.apache.rocketmq.broker.service.BrokerMessageService;
-import org.apache.rocketmq.broker.out.BrokerOuterAPI;
 import org.apache.rocketmq.broker.slave.SlaveSynchronize;
 import org.apache.rocketmq.broker.topic.TopicConfigManager;
 import org.apache.rocketmq.common.BrokerConfig;
 import org.apache.rocketmq.common.Pair;
-import org.apache.rocketmq.common.UtilAll;
 import org.apache.rocketmq.common.utils.IOTinyUtils;
 import org.apache.rocketmq.remoting.protocol.header.controller.ElectMasterResponseHeader;
 import org.apache.rocketmq.remoting.protocol.header.controller.GetMetaDataResponseHeader;
@@ -356,7 +355,7 @@ public class ReplicasManagerRegisterTest {
 
     @After
     public void clear() {
-        UtilAll.deleteFile(new File(STORE_BASE_PATH));
+        IOTinyUtils.deleteFile(new File(STORE_BASE_PATH));
     }
 
 
