f315753b40bd6baf52facf95ab2f3635b5ef49e2
wenliang <twlls2011@163.com>
2023-10-11 19:45:22 +0800

    refactor: UtilAll.frontStringAtLeast to StringUtils

--- tools/src/main/java/org/apache/rocketmq/tools/command/broker/BrokerConsumeStatsSubCommad.java
+++ tools/src/main/java/org/apache/rocketmq/tools/command/broker/BrokerConsumeStatsSubCommad.java
@@ -27,6 +27,7 @@ import org.apache.commons.cli.Options;
 import org.apache.rocketmq.common.UtilAll;
 import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.common.utils.DateUtils;
+import org.apache.rocketmq.common.utils.StringUtils;
 import org.apache.rocketmq.remoting.RPCHook;
 import org.apache.rocketmq.remoting.protocol.admin.ConsumeStats;
 import org.apache.rocketmq.remoting.protocol.admin.OffsetWrapper;
@@ -138,7 +139,7 @@ public class BrokerConsumeStatsSubCommad implements SubCommand {
                             }
                             if (offsetWrapper.getLastTimestamp() > 0)
                                 System.out.printf("%-64s  %-64s  %-32s  %-4d  %-20d  %-20d  %-20d  %s%n",
-                                    UtilAll.frontStringAtLeast(mq.getTopic(), 64),
+                                    StringUtils.frontStringAtLeast(mq.getTopic(), 64),
                                     group,
                                     UtilAll.frontStringAtLeast(mq.getBrokerName(), 32),
                                     mq.getQueueId(),
