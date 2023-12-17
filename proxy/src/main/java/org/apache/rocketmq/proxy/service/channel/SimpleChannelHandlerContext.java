6480d6f5785ce3a3cf30cf2c9c1d2e568afaa559
winglechen <winglechen@gmail.com>
2023-12-17 21:38:16 +0800

    delete: delete NotImplementedException StopWatch

--- proxy/src/main/java/org/apache/rocketmq/proxy/service/channel/SimpleChannelHandlerContext.java
+++ proxy/src/main/java/org/apache/rocketmq/proxy/service/channel/SimpleChannelHandlerContext.java
@@ -29,7 +29,7 @@ import io.netty.util.Attribute;
 import io.netty.util.AttributeKey;
 import io.netty.util.concurrent.EventExecutor;
 import java.net.SocketAddress;
-import org.apache.rocketmq.common.lang.NotImplementedException;
+import org.apache.commons.lang3.NotImplementedException;
 
 public class SimpleChannelHandlerContext implements ChannelHandlerContext {
 
