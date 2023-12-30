fc7d529b88290156029974584a195831257dd690
winglechen <winglechen@gmail.com>
2023-12-30 16:55:02 +0800

    test: add test file

---
+++ apitest/src/main/java/org/apache/rocketmq/apitest/PushConsumerExample.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.rocketmq.apitest;
+
+import java.io.IOException;
+import java.util.Collections;
+import org.apache.rocketmq.client.apis.ClientConfiguration;
+import org.apache.rocketmq.client.apis.ClientException;
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
+import org.apache.rocketmq.client.apis.SessionCredentialsProvider;
+import org.apache.rocketmq.client.apis.StaticSessionCredentialsProvider;
+import org.apache.rocketmq.client.apis.consumer.ConsumeResult;
+import org.apache.rocketmq.client.apis.consumer.FilterExpression;
+import org.apache.rocketmq.client.apis.consumer.FilterExpressionType;
+import org.apache.rocketmq.client.apis.consumer.PushConsumer;
+import org.apache.rocketmq.logging.org.slf4j.Logger;
+import org.apache.rocketmq.logging.org.slf4j.LoggerFactory;
+
+public class PushConsumerExample {
+    private static final Logger log = LoggerFactory.getLogger(PushConsumerExample.class);
+
+    private PushConsumerExample() {
+    }
+
+    public static void main(String[] args) throws ClientException, InterruptedException, IOException {
+        final ClientServiceProvider provider = ClientServiceProvider.loadService();
+
+        // Credential provider is optional for client configuration.
+        String accessKey = "yourAccessKey";
+        String secretKey = "yourSecretKey";
+        SessionCredentialsProvider sessionCredentialsProvider =
+            new StaticSessionCredentialsProvider(accessKey, secretKey);
+
+        String endpoints = "192.168.50.159:8080";
+        ClientConfiguration clientConfiguration = ClientConfiguration.newBuilder()
+            .setEndpoints(endpoints)
+            // On some Windows platforms, you may encounter SSL compatibility issues. Try turning off the SSL option in
+            // client configuration to solve the problem please if SSL is not essential.
+            .enableSsl(false)
+            //.setCredentialProvider(sessionCredentialsProvider)
+            .build();
+        String tag = "*";
+        FilterExpression filterExpression = new FilterExpression(tag, FilterExpressionType.TAG);
+        String consumerGroup = "MQ_TEST_GROUP";
+        String topic = "MQ_TEST_TOPIC";
+        // In most case, you don't need to create too many consumers, singleton pattern is recommended.
+        PushConsumer pushConsumer = provider.newPushConsumerBuilder()
+            .setClientConfiguration(clientConfiguration)
+            // Set the consumer group name.
+            .setConsumerGroup(consumerGroup)
+            // Set the subscription for the consumer.
+            .setSubscriptionExpressions(Collections.singletonMap(topic, filterExpression))
+            .setMessageListener(messageView -> {
+                // Handle the received message and return consume result.
+                log.error("Consume message={}", messageView);
+                System.out.println("consume message= " + messageView);
+                return ConsumeResult.SUCCESS;
+            })
+            .build();
+        // Block the main thread, no need for production environment.
+        Thread.sleep(10000);
+        // Close the push consumer when you don't need it anymore.
+        // You could close it manually or add this into the JVM shutdown hook.
+        pushConsumer.close();
+    }
+}
