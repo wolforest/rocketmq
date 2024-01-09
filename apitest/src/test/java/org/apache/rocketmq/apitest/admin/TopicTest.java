8c4038a920d8ce3487f369a8d7f57d66a3219524
winglechen <winglechen@gmail.com>
2024-01-09 17:25:21 +0800

    style: fix style bugs

--- apitest/src/test/java/org/apache/rocketmq/apitest/admin/TopicTest.java
+++ apitest/src/test/java/org/apache/rocketmq/apitest/admin/TopicTest.java
@@ -27,7 +27,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-@Test( groups = {"admin"})
+@Test(groups = {"admin"})
 public class TopicTest extends ApiBaseTest {
     private static final Logger LOG = LoggerFactory.getLogger(TopicTest.class);
 
