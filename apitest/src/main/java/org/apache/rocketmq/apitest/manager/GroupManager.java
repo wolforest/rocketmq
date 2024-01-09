0606e158fdf6e1d3b28bb633da3f542760ab5c87
winglechen <winglechen@gmail.com>
2024-01-09 10:38:23 +0800

    feature: add GroupManager

---
+++ apitest/src/main/java/org/apache/rocketmq/apitest/manager/GroupManager.java
@@ -0,0 +1,42 @@
+package org.apache.rocketmq.apitest.manager;
+
+import org.apache.rocketmq.remoting.protocol.subscription.SubscriptionGroupConfig;
+
+public class GroupManager {
+    private static final String GROUP_PREFIX = "MQG_";
+
+    public static boolean createGroup(String group) {
+        SubscriptionGroupConfig config = new SubscriptionGroupConfig();
+        config.setGroupName(group);
+
+        String brokerAddr = ConfigManager.getConfig().getString("brokerAddr");
+        try {
+            ClientManager.getClient().createSubscriptionGroup(brokerAddr, config);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public static SubscriptionGroupConfig getGroup(String group) {
+        try {
+            String brokerAddr = ConfigManager.getConfig().getString("brokerAddr");
+            return ClientManager.getClient().getSubscriptionGroupConfig(brokerAddr, group);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void deleteGroup(String group) {
+        try {
+            String brokerAddr = ConfigManager.getConfig().getString("brokerAddr");
+            ClientManager.getClient().deleteSubscriptionGroup(brokerAddr, group, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- apitest/src/test/java/org/apache/rocketmq/apitest/admin/GroupTest.java
+++ apitest/src/test/java/org/apache/rocketmq/apitest/admin/GroupTest.java
@@ -1,4 +1,13 @@
 package org.apache.rocketmq.apitest.admin;
 
-public class GroupTest {
+import org.apache.rocketmq.apitest.ApiBaseTest;
+import org.testng.annotations.Test;
+
+@Test(groups = "admin")
+public class GroupTest extends ApiBaseTest {
+
+    @Test
+    public void testAddAndDeleteTest() {
+
+    }
 }
