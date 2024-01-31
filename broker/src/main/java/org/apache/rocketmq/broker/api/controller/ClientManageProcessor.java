d8050e8204183858c1384c9e30e5e4ece07c881a
winglechen <winglechen@gmail.com>
2024-01-31 17:45:34 +0800

    refactor: simplify ClientManageProcessor

--- broker/src/main/java/org/apache/rocketmq/broker/api/controller/ClientManageProcessor.java
+++ broker/src/main/java/org/apache/rocketmq/broker/api/controller/ClientManageProcessor.java
@@ -45,6 +45,20 @@ import org.apache.rocketmq.remoting.protocol.heartbeat.ProducerData;
 import org.apache.rocketmq.remoting.protocol.heartbeat.SubscriptionData;
 import org.apache.rocketmq.remoting.protocol.subscription.SubscriptionGroupConfig;
 
+/**
+ * handle client heartbeat、unregister, checkClientConfig requests
+ * in heartbeat:
+ *  - registerProducer
+ *  - registerConsumer
+ *
+ * in unregister:
+ *  - unregisterProducer
+ *  - unregisterConsumer
+ *
+ * in check client config:
+ *  - check subscription data
+ *
+ */
 public class ClientManageProcessor implements NettyRequestProcessor {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggerName.BROKER_LOGGER_NAME);
     private final Broker broker;
@@ -55,15 +69,14 @@ public class ClientManageProcessor implements NettyRequestProcessor {
     }
 
     @Override
-    public RemotingCommand processRequest(ChannelHandlerContext ctx, RemotingCommand request)
-        throws RemotingCommandException {
+    public RemotingCommand processRequest(ChannelHandlerContext ctx, RemotingCommand request) throws RemotingCommandException {
         switch (request.getCode()) {
             case RequestCode.HEART_BEAT:
                 return this.heartBeat(ctx, request);
             case RequestCode.UNREGISTER_CLIENT:
                 return this.unregisterClient(ctx, request);
             case RequestCode.CHECK_CLIENT_CONFIG:
-                return this.checkClientConfig(ctx, request);
+                return this.checkClientConfig(request);
             default:
                 break;
         }
@@ -78,70 +91,17 @@ public class ClientManageProcessor implements NettyRequestProcessor {
     public RemotingCommand heartBeat(ChannelHandlerContext ctx, RemotingCommand request) {
         RemotingCommand response = RemotingCommand.createResponseCommand(null);
         HeartbeatData heartbeatData = HeartbeatData.decode(request.getBody(), HeartbeatData.class);
-        ClientChannelInfo clientChannelInfo = new ClientChannelInfo(
-            ctx.channel(),
-            heartbeatData.getClientID(),
-            request.getLanguage(),
-            request.getVersion()
-        );
-        int heartbeatFingerprint = heartbeatData.getHeartbeatFingerprint();
-        if (heartbeatFingerprint != 0) {
+        ClientChannelInfo clientChannelInfo = new ClientChannelInfo(ctx.channel(), heartbeatData.getClientID(), request.getLanguage(), request.getVersion());
+
+        int fingerprint = heartbeatData.getHeartbeatFingerprint();
+        if (fingerprint != 0) {
             return heartBeatV2(ctx, heartbeatData, clientChannelInfo, response);
         }
-        for (ConsumerData consumerData : heartbeatData.getConsumerDataSet()) {
-            //Reject the PullConsumer
-            if (broker.getBrokerConfig().isRejectPullConsumerEnable()) {
-                if (ConsumeType.CONSUME_ACTIVELY == consumerData.getConsumeType()) {
-                    continue;
-                }
-            }
-            consumerGroupHeartbeatTable.put(consumerData.getGroupName(), heartbeatFingerprint);
-            boolean hasOrderTopicSub = false;
 
-            for (final SubscriptionData subscriptionData : consumerData.getSubscriptionDataSet()) {
-                if (this.broker.getTopicConfigManager().isOrderTopic(subscriptionData.getTopic())) {
-                    hasOrderTopicSub = true;
-                    break;
-                }
-            }
 
-            SubscriptionGroupConfig subscriptionGroupConfig = this.broker.getSubscriptionGroupManager()
-                .findSubscriptionGroupConfig(consumerData.getGroupName());
-            boolean isNotifyConsumerIdsChangedEnable = true;
+        registerConsumer(ctx, heartbeatData, fingerprint, clientChannelInfo);
+        registerProducer(heartbeatData, clientChannelInfo);
 
-            if (null == subscriptionGroupConfig) {
-                continue;
-            }
-
-            isNotifyConsumerIdsChangedEnable = subscriptionGroupConfig.isNotifyConsumerIdsChangedEnable();
-            int topicSysFlag = 0;
-            if (consumerData.isUnitMode()) {
-                topicSysFlag = TopicSysFlag.buildSysFlag(false, true);
-            }
-            String newTopic = MQConstants.getRetryTopic(consumerData.getGroupName());
-            this.broker.getTopicConfigManager().createTopicInSendMessageBackMethod(newTopic, subscriptionGroupConfig.getRetryQueueNums(),
-                PermName.PERM_WRITE | PermName.PERM_READ, hasOrderTopicSub, topicSysFlag);
-
-            boolean changed = this.broker.getConsumerManager().registerConsumer(
-                consumerData.getGroupName(),
-                clientChannelInfo,
-                consumerData.getConsumeType(),
-                consumerData.getMessageModel(),
-                consumerData.getConsumeFromWhere(),
-                consumerData.getSubscriptionDataSet(),
-                isNotifyConsumerIdsChangedEnable
-            );
-            if (changed) {
-                LOGGER.info("ClientManageProcessor: registerConsumer info changed, SDK address={}, consumerData={}",
-                    RemotingHelper.parseChannelRemoteAddr(ctx.channel()), consumerData.toString());
-            }
-
-        }
-
-        for (ProducerData data : heartbeatData.getProducerDataSet()) {
-            this.broker.getProducerManager().registerProducer(data.getGroupName(),
-                clientChannelInfo);
-        }
         response.setCode(ResponseCode.SUCCESS);
         response.setRemark(null);
         response.addExtField(MQConstants.IS_SUPPORT_HEART_BEAT_V2, Boolean.TRUE.toString());
@@ -149,7 +109,58 @@ public class ClientManageProcessor implements NettyRequestProcessor {
         return response;
     }
 
+
     private RemotingCommand heartBeatV2(ChannelHandlerContext ctx, HeartbeatData heartbeatData, ClientChannelInfo clientChannelInfo, RemotingCommand response) {
+        boolean isSubChange = registerConsumerV2(ctx, heartbeatData, clientChannelInfo);
+        registerProducer(heartbeatData, clientChannelInfo);
+
+        response.setCode(ResponseCode.SUCCESS);
+        response.setRemark(null);
+        response.addExtField(MQConstants.IS_SUPPORT_HEART_BEAT_V2, Boolean.TRUE.toString());
+        response.addExtField(MQConstants.IS_SUB_CHANGE, Boolean.valueOf(isSubChange).toString());
+        return response;
+    }
+
+    private void registerConsumer(ChannelHandlerContext ctx, HeartbeatData heartbeatData, int fingerprint, ClientChannelInfo clientChannelInfo) {
+        for (ConsumerData consumerData : heartbeatData.getConsumerDataSet()) {
+            //Reject the PullConsumer
+            if (broker.getBrokerConfig().isRejectPullConsumerEnable()) {
+                if (ConsumeType.CONSUME_ACTIVELY == consumerData.getConsumeType()) {
+                    continue;
+                }
+            }
+            consumerGroupHeartbeatTable.put(consumerData.getGroupName(), fingerprint);
+
+            boolean hasOrderTopicSub = false;
+            for (final SubscriptionData subscriptionData : consumerData.getSubscriptionDataSet()) {
+                if (this.broker.getTopicConfigManager().isOrderTopic(subscriptionData.getTopic())) {
+                    hasOrderTopicSub = true;
+                    break;
+                }
+            }
+
+            SubscriptionGroupConfig subscriptionGroupConfig = this.broker.getSubscriptionGroupManager().findSubscriptionGroupConfig(consumerData.getGroupName());
+            if (null == subscriptionGroupConfig) {
+                continue;
+            }
+
+            boolean isNotifyConsumerIdsChangedEnable = subscriptionGroupConfig.isNotifyConsumerIdsChangedEnable();
+            int topicSysFlag = 0;
+            if (consumerData.isUnitMode()) {
+                topicSysFlag = TopicSysFlag.buildSysFlag(false, true);
+            }
+            String newTopic = MQConstants.getRetryTopic(consumerData.getGroupName());
+            this.broker.getTopicConfigManager().createTopicInSendMessageBackMethod(newTopic, subscriptionGroupConfig.getRetryQueueNums(), PermName.PERM_WRITE | PermName.PERM_READ, hasOrderTopicSub, topicSysFlag);
+
+            boolean changed = this.broker.getConsumerManager().registerConsumer(consumerData.getGroupName(), clientChannelInfo, consumerData.getConsumeType(), consumerData.getMessageModel(), consumerData.getConsumeFromWhere(), consumerData.getSubscriptionDataSet(), isNotifyConsumerIdsChangedEnable);
+            if (changed) {
+                LOGGER.info("ClientManageProcessor: registerConsumer info changed, SDK address={}, consumerData={}", RemotingHelper.parseChannelRemoteAddr(ctx.channel()), consumerData.toString());
+            }
+
+        }
+    }
+
+    private boolean registerConsumerV2(ChannelHandlerContext ctx, HeartbeatData heartbeatData, ClientChannelInfo clientChannelInfo) {
         boolean isSubChange = false;
         for (ConsumerData consumerData : heartbeatData.getConsumerDataSet()) {
             //Reject the PullConsumer
@@ -171,112 +182,98 @@ public class ClientManageProcessor implements NettyRequestProcessor {
                 }
             }
             SubscriptionGroupConfig subscriptionGroupConfig = this.broker.getSubscriptionGroupManager().findSubscriptionGroupConfig(consumerData.getGroupName());
-            boolean isNotifyConsumerIdsChangedEnable = true;
             if (null == subscriptionGroupConfig) {
                 continue;
             }
-            isNotifyConsumerIdsChangedEnable = subscriptionGroupConfig.isNotifyConsumerIdsChangedEnable();
+            boolean isNotifyConsumerIdsChangedEnable = subscriptionGroupConfig.isNotifyConsumerIdsChangedEnable();
             int topicSysFlag = 0;
             if (consumerData.isUnitMode()) {
                 topicSysFlag = TopicSysFlag.buildSysFlag(false, true);
             }
             String newTopic = MQConstants.getRetryTopic(consumerData.getGroupName());
             this.broker.getTopicConfigManager().createTopicInSendMessageBackMethod(newTopic, subscriptionGroupConfig.getRetryQueueNums(), PermName.PERM_WRITE | PermName.PERM_READ, hasOrderTopicSub, topicSysFlag);
-            boolean changed = false;
+            boolean changed;
             if (heartbeatData.isWithoutSub()) {
                 changed = this.broker.getConsumerManager().registerConsumerWithoutSub(consumerData.getGroupName(), clientChannelInfo, consumerData.getConsumeType(), consumerData.getMessageModel(), consumerData.getConsumeFromWhere(), isNotifyConsumerIdsChangedEnable);
             } else {
                 changed = this.broker.getConsumerManager().registerConsumer(consumerData.getGroupName(), clientChannelInfo, consumerData.getConsumeType(), consumerData.getMessageModel(), consumerData.getConsumeFromWhere(), consumerData.getSubscriptionDataSet(), isNotifyConsumerIdsChangedEnable);
             }
             if (changed) {
-                LOGGER.info("heartBeatV2 ClientManageProcessor: registerConsumer info changed, SDK address={}, consumerData={}",
-                        RemotingHelper.parseChannelRemoteAddr(ctx.channel()), consumerData.toString());
+                LOGGER.info("heartBeatV2 ClientManageProcessor: registerConsumer info changed, SDK address={}, consumerData={}", RemotingHelper.parseChannelRemoteAddr(ctx.channel()), consumerData.toString());
             }
 
         }
+        return isSubChange;
+    }
+
+    private void registerProducer(HeartbeatData heartbeatData, ClientChannelInfo clientChannelInfo) {
         for (ProducerData data : heartbeatData.getProducerDataSet()) {
             this.broker.getProducerManager().registerProducer(data.getGroupName(), clientChannelInfo);
         }
-        response.setCode(ResponseCode.SUCCESS);
-        response.setRemark(null);
-        response.addExtField(MQConstants.IS_SUPPORT_HEART_BEAT_V2, Boolean.TRUE.toString());
-        response.addExtField(MQConstants.IS_SUB_CHANGE, Boolean.valueOf(isSubChange).toString());
-        return response;
     }
 
-    public RemotingCommand unregisterClient(ChannelHandlerContext ctx, RemotingCommand request)
-        throws RemotingCommandException {
-        final RemotingCommand response =
-            RemotingCommand.createResponseCommand(UnregisterClientResponseHeader.class);
-        final UnregisterClientRequestHeader requestHeader =
-            (UnregisterClientRequestHeader) request
-                .decodeCommandCustomHeader(UnregisterClientRequestHeader.class);
+    public RemotingCommand unregisterClient(ChannelHandlerContext ctx, RemotingCommand request) throws RemotingCommandException {
+        RemotingCommand response = RemotingCommand.createResponseCommand(UnregisterClientResponseHeader.class);
+        UnregisterClientRequestHeader requestHeader = (UnregisterClientRequestHeader) request.decodeCommandCustomHeader(UnregisterClientRequestHeader.class);
+        ClientChannelInfo clientChannelInfo = new ClientChannelInfo(ctx.channel(), requestHeader.getClientID(), request.getLanguage(), request.getVersion());
 
-        ClientChannelInfo clientChannelInfo = new ClientChannelInfo(
-            ctx.channel(),
-            requestHeader.getClientID(),
-            request.getLanguage(),
-            request.getVersion());
-        {
-            final String group = requestHeader.getProducerGroup();
-            if (group != null) {
-                this.broker.getProducerManager().unregisterProducer(group, clientChannelInfo);
-            }
-        }
+        unregisterProducer(requestHeader, clientChannelInfo);
+        unregisterConsumer(requestHeader, clientChannelInfo);
 
-        {
-            final String group = requestHeader.getConsumerGroup();
-            if (group != null) {
-                SubscriptionGroupConfig subscriptionGroupConfig =
-                    this.broker.getSubscriptionGroupManager().findSubscriptionGroupConfig(group);
-                boolean isNotifyConsumerIdsChangedEnable = true;
-                if (null != subscriptionGroupConfig) {
-                    isNotifyConsumerIdsChangedEnable = subscriptionGroupConfig.isNotifyConsumerIdsChangedEnable();
-                }
-                this.broker.getConsumerManager().unregisterConsumer(group, clientChannelInfo, isNotifyConsumerIdsChangedEnable);
-            }
-        }
-
-        response.setCode(ResponseCode.SUCCESS);
-        response.setRemark(null);
-        return response;
+        return response.setCodeAndRemark(ResponseCode.SUCCESS, null);
     }
 
-    public RemotingCommand checkClientConfig(ChannelHandlerContext ctx, RemotingCommand request)
-        throws RemotingCommandException {
-        final RemotingCommand response = RemotingCommand.createResponseCommand(null);
-
-        CheckClientRequestBody requestBody = CheckClientRequestBody.decode(request.getBody(),
-            CheckClientRequestBody.class);
-
-        if (requestBody != null && requestBody.getSubscriptionData() != null) {
-            SubscriptionData subscriptionData = requestBody.getSubscriptionData();
-
-            if (ExpressionType.isTagType(subscriptionData.getExpressionType())) {
-                response.setCode(ResponseCode.SUCCESS);
-                response.setRemark(null);
-                return response;
-            }
-
-            if (!this.broker.getBrokerConfig().isEnablePropertyFilter()) {
-                response.setCode(ResponseCode.SYSTEM_ERROR);
-                response.setRemark("The broker does not support consumer to filter message by " + subscriptionData.getExpressionType());
-                return response;
-            }
-
-            try {
-                FilterFactory.INSTANCE.get(subscriptionData.getExpressionType()).compile(subscriptionData.getSubString());
-            } catch (Exception e) {
-                LOGGER.warn("Client {}@{} filter message, but failed to compile expression! sub={}, error={}",
-                    requestBody.getClientId(), requestBody.getGroup(), requestBody.getSubscriptionData(), e.getMessage());
-                response.setCode(ResponseCode.SUBSCRIPTION_PARSE_FAILED);
-                response.setRemark(e.getMessage());
-                return response;
-            }
+    private void unregisterProducer(UnregisterClientRequestHeader requestHeader, ClientChannelInfo clientChannelInfo) {
+        final String group = requestHeader.getProducerGroup();
+        if (group == null) {
+            return;
         }
 
-        response.setCode(ResponseCode.SUCCESS);
-        response.setRemark(null);
-        return response;
+        this.broker.getProducerManager().unregisterProducer(group, clientChannelInfo);
+    }
+
+    private void unregisterConsumer(UnregisterClientRequestHeader requestHeader, ClientChannelInfo clientChannelInfo) {
+        final String group = requestHeader.getConsumerGroup();
+        if (group == null) {
+            return;
+        }
+
+        SubscriptionGroupConfig subscriptionGroupConfig = this.broker.getSubscriptionGroupManager().findSubscriptionGroupConfig(group);
+        boolean isNotifyConsumerIdsChangedEnable = true;
+        if (null != subscriptionGroupConfig) {
+            isNotifyConsumerIdsChangedEnable = subscriptionGroupConfig.isNotifyConsumerIdsChangedEnable();
+        }
+        this.broker.getConsumerManager().unregisterConsumer(group, clientChannelInfo, isNotifyConsumerIdsChangedEnable);
+    }
+
+    public RemotingCommand checkClientConfig(RemotingCommand request) {
+        RemotingCommand response = RemotingCommand.createResponseCommand(null);
+        CheckClientRequestBody requestBody = CheckClientRequestBody.decode(request.getBody(), CheckClientRequestBody.class);
+        if (requestBody == null || requestBody.getSubscriptionData() == null) {
+            return response.setCodeAndRemark(ResponseCode.SUCCESS, null);
+        }
+
+        SubscriptionData subscriptionData = requestBody.getSubscriptionData();
+        if (ExpressionType.isTagType(subscriptionData.getExpressionType())) {
+            response.setCode(ResponseCode.SUCCESS);
+            response.setRemark(null);
+            return response;
+        }
+
+        if (!this.broker.getBrokerConfig().isEnablePropertyFilter()) {
+            response.setCode(ResponseCode.SYSTEM_ERROR);
+            response.setRemark("The broker does not support consumer to filter message by " + subscriptionData.getExpressionType());
+            return response;
+        }
+
+        try {
+            FilterFactory.INSTANCE.get(subscriptionData.getExpressionType()).compile(subscriptionData.getSubString());
+        } catch (Exception e) {
+            LOGGER.warn("Client {}@{} filter message, but failed to compile expression! sub={}, error={}",
+                requestBody.getClientId(), requestBody.getGroup(), requestBody.getSubscriptionData(), e.getMessage());
+            return response.setCodeAndRemark(ResponseCode.SUBSCRIPTION_PARSE_FAILED, e.getMessage());
+        }
+
+        return response.setCodeAndRemark(ResponseCode.SUCCESS, null);
     }
 }
